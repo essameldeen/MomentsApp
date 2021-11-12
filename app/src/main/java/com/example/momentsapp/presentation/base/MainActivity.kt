729432471bf15db893123e438e8c867a939dcfb1
essameldeen <essameldeen.ibrahim.stdsw@cis.asu.edu.eg>
2021-11-12 22:47:45 +0200

    4# link bottomNavigation with navHost

--- app/src/main/java/com/example/momentsapp/presentation/base/MainActivity.kt
+++ app/src/main/java/com/example/momentsapp/presentation/base/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.momentsapp.presentation.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.momentsapp.R
+import com.example.momentsapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setupWithNavController(
+            navController
+        )
+
     }
 }
