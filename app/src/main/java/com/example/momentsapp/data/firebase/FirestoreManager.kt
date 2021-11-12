bf920a2f533b83d948b6331c7738f01ad3b54102
mohamed <mohamed.ashraf.10107@gmail.com>
2021-11-12 22:53:44 +0200

    Commit: retrieving missing implementations.

--- app/src/main/java/com/example/momentsapp/data/firebase/FirestoreManager.kt
+++ app/src/main/java/com/example/momentsapp/data/firebase/FirestoreManager.kt
@@ -2,11 +2,11 @@ package com.example.momentsapp.data.firebase
 
 import com.example.momentsapp.data.model.Moment
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
-class FirestoreManager {
-    fun addMoment(moment: Moment, listener: (Boolean) -> Unit) {
+class FirestoreManager
+{
+    fun addMoment(moment: Moment, listener: (Boolean) -> Unit)
+    {
         val db = FirebaseFirestore.getInstance()
         db.collection("moments").document(moment.id).set(
             mapOf(
@@ -26,49 +26,38 @@ class FirestoreManager {
         }
     }
 
-    fun listenToMoments(listener: (MutableList<Moment>) -> Unit) {
+    fun listenToMoments(listener: (MutableList<Moment>) -> Unit)
+    {
         val db = FirebaseFirestore.getInstance()
-        try {
+        try
+        {
             val reference = db.collection("moments")
             reference.addSnapshotListener { snapshot, e ->
-                if (e != null) {
-                    e.printStackTrace()
-                } else if (snapshot != null && !snapshot.isEmpty) {
+                if (e != null) e.printStackTrace()
+                else if (snapshot != null && !snapshot.isEmpty)
+                {
                     val moments = mutableListOf<Moment>()
-                    for (document in snapshot.documents) {
+                    for (document in snapshot.documents)
+                    {
                         val data = document.data
-                        if (data != null) {
-                            for (momentItem in data.values) {
-                                val allMoments = momentItem as ArrayList<HashMap<String, Any?>>
-
-                                for (item in allMoments) {
-                                    val id = item["id"].toString()
-                                    val name = item["name"].toString()
-                                    val description = item["description"].toString()
-                                    val photo = item["photo"].toString()
-                                    val country = item["country"].toString()
-                                    val city = item["city"].toString()
-                                    val likes = item["likes"].toString().toLong()
-
-                                    moments.add(
-                                        Moment(
-                                            id,
-                                            name,
-                                            description,
-                                            photo,
-                                            country,
-                                            city,
-                                            likes
-                                        )
-                                    )
-                                }
-                            }
+                        if (data != null)
+                        {
+                            val id = data["id"].toString()
+                            val name = data["name"].toString()
+                            val description = data["description"].toString()
+                            val photo = data["photo"].toString()
+                            val country = data["country"].toString()
+                            val city = data["city"].toString()
+                            val likes = data["likes"].toString().toLong()
+                            moments.add(Moment(id, name, description, photo, country, city, likes))
                         }
                     }
                     listener(moments)
                 }
             }
-        } catch (ex: Exception) {
+        }
+        catch (ex: Exception)
+        {
             ex.printStackTrace()
         }
     }
--- app/src/main/java/com/example/momentsapp/data/repository/MomentRepositoryImplementation.kt
+++ app/src/main/java/com/example/momentsapp/data/repository/MomentRepositoryImplementation.kt
@@ -3,23 +3,17 @@ package com.example.momentsapp.data.repository
 import com.example.momentsapp.data.firebase.FirestoreManager
 import com.example.momentsapp.data.model.Moment
 import com.example.momentsapp.domain.repository.MomentRepository
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.getValue
 
-class MomentRepositoryImplementation constructor(
-    private val database: FirestoreManager
-) : MomentRepository {
-
-    override fun createMoment(moment: Moment, listener: (Boolean) -> Unit) {}
-
+class MomentRepositoryImplementation constructor(private val database: FirestoreManager) : MomentRepository
+{
     private var moments = mutableListOf<Moment>()
 
     override fun getCachedMoments(): MutableList<Moment> = moments
 
-    override fun getMoments(listener: (MutableList<Moment>) -> Unit){
+    override fun createMoment(moment: Moment, listener: (Boolean) -> Unit) = database.addMoment(moment, listener)
+
+    override fun getMoments(listener: (MutableList<Moment>) -> Unit)
+    {
         database.listenToMoments {
             moments = it
             listener(moments)
